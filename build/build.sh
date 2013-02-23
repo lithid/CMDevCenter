f34d0cf2b10ebbb47f547000f3fea24b356ccdd5
lithid <long.jeremie@gmail.com>
2013-02-23 13:09:21 -0500

    Removed scripts

--- build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dpkg-buildpackage -rfakeroot
-
-if [ $? -eq 0 ]; then
-    gdialog --title "Success" --msgbox "deb created" 200 200
-else
-    gdialog --title "Error" --msgbox "deb not created" 200 200
-fi
